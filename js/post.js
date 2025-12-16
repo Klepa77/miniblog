940f19a75084ba1d0c7384c7ae37d0ba767fc2b6
Klepa77 <gonza367@mail.ru>
2025-12-16 18:39:53 +0300

    minorfix

--- js/post.js
+++ js/post.js
@@ -1,6 +1,8 @@
 const params = new URLSearchParams(window.location.search)
 const id = params.get('id')
 let commentsContainer = document.querySelector('.comments-container')
+let deleteId = ''
+let editId = ''
 
 auth.onAuthStateChanged(user => {
   if (user) {
@@ -93,7 +95,7 @@ let query = getComments()
 
 
 query.then((snapchot) => {
-  commentCount.textContent = snapchot.size
+  // commentCount.textContent = snapchot.size
   snapchot.forEach((doc) => {
     // Создать функцию генерации и отьрисовки комментариев на экране
     renderComments(doc.data())
@@ -107,15 +109,90 @@ function getComments() {
 }
 
 function renderComments(comment) {
-
   let divComment = document.createElement('div')
+  let userData = document.createElement('div')
+  let btnEdit = document.createElement('button')
+  let btnDelete = document.createElement('button')
+  let btnContainer = document.createElement('div')
+  btnContainer.className = 'btn-container'
+  divComment.id = "_" + comment.id
+
+  btnDelete.className = 'btn btn-danger btn-delete'
+  btnEdit.className = 'btn btn-primary btn-edit'
+  btnEdit.textContent = 'edit'
+  btnDelete.textContent = 'delete'
+  btnDelete.id = "_" + comment.id
+  btnEdit.id = "_" + comment.id
+
+  btnContainer.appendChild(btnEdit)
+  btnContainer.appendChild(btnDelete)
+
+  userData.className = 'user-data'
   divComment.className = 'comment'
+
   let author = document.createElement('strong')
+
   author.textContent = comment.author
-  divComment.appendChild(author)
+  userData.appendChild(author)
+  userData.appendChild(btnContainer)
+  divComment.appendChild(userData)
+
   let text = document.createElement('p')
   text.textContent = comment.text
   divComment.appendChild(text)
 
   commentsContainer.appendChild(divComment)
+
+  btnEdit.addEventListener('click', function () {
+
+  })
+
+  btnDelete.addEventListener('click', function (e) {
+    deleteId = e.target.id
+    console.log(deleteId)
+    openDeleteModal()
+  })
 }
+
+
+let deleteModal = document.getElementById('deleteModal')
+let closeBtn = deleteModal.querySelector('.btn-cancel')
+
+function openDeleteModal() {
+  deleteModal.style.display = 'flex'
+}
+
+function closeDeleteModal() {
+  deleteModal.style.display = 'none'
+}
+
+closeBtn.onclick = closeDeleteModal
+
+function confirmDelete() {
+  auth.onAuthStateChanged(user => {
+    db.collection('comments').doc(deleteId).delete()
+  })
+  closeDeleteModal()
+}
+auth.onAuthStateChanged(user => {
+  if (user) {
+    db.collection('comments').onSnapshot(snapshot => {
+      let changes = snapshot.docChanges()
+      changes.forEach(el => {
+        let parentDiv = document.getElementById(el.doc.id)
+        if (el.type == 'removed') {
+          parentDiv.remove()
+          console.log('removed')
+        } else if (el.type === 'added') {
+          renderData({ ...el.doc.data(), id: el.doc.id })
+        } else if (el.type == 'modified') {
+          parentDiv.remove()
+          renderData({ ...el.doc.data(), id: el.doc.id })
+        }
+      })
+      query.then((snapchot) => {
+        commentCount.textContent = snapchot.size
+      })
+    })
+  }
+})
